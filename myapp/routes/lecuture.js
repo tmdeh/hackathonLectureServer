5f936dc26621286b08c32f8149876ae39e011385
tmdeh <yus174113@gmail.com>
2021-07-13 23:11:46 +0900

    특강신청

--- myapp/routes/lecuture.js
+++ myapp/routes/lecuture.js
@@ -203,16 +203,104 @@ router.get('/user/:userId', function(req, res, next) {
 });
 
 router.post('/search', function(req, res, next) {
-
+    let now = new Date();
+    let search = '%' + req.body.title + '%';
+    console.log(search);
+    db.query("SELECT * FROM Lecture WHERE title LIKE ?", [search], (err, result) => {
+        if(result == undefined || err || Object.keys(result).length == 0){
+            res.status(404).json({
+                data : null,
+                message : "게시물이 존재하지 않습니다."
+            })
+        }
+        else{
+            let resData = [];
+            for(let i = 0; i < result.length; i++){
+                let url = result[i].attachment_url.split(',');
+                let field = JSON.parse(result[i].field);
+                let state  =  -1;
+                if(now < result[i].proposal){//모집 중
+                    state = 0;
+                }
+                if (now >= result[i].start_date){//모집 종료 진행중
+                    state = 1;
+                }
+                if(now > result[i].end_date){//종료됨
+                    state = 2;
+                }
+                let data = {
+                    lecture_id : result[0].lecture_id,
+                    title : result[0].title,
+                    content : result[0].content,
+                    attachmentUrl : url,
+                    userId : result[0].user_id,
+                    field : field,
+                    startDate : result[0].start_date,
+                    endDate : result[0].end_date,
+                    uploadDate : result[0].upload_date,
+                    proposal : result[0].proposal,
+                    state : state,
+                }
+                resData.push(data);
+            }
+            res.status(200).json({
+                data : resData,
+                message : "게시물 가져오기를 성공하였습니다."
+            })
+        }
+    })
 });
 
 router.post('/proposal', function(req, res, next) {
+    let lectureId = req.body.lectureId;
+    const token = req.get('authorization');
+    const tokendecode = () => {
+    const promise = new Promise((resolve, reject) => {
+      jwt.verify(token, secret, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      })
+    })
+    return promise;
+  }
 
+  const insertQuery = (data) => {
+    const promise = new Promise((resolve, reject) => {
+        db.query('INSERT INTO `hackathon_lecture`.`Student` (`user_id_fk`, `lecture_id_fk`) VALUES (?, ?)', [data.userId, lectureId], (error, result) => {
+            if(error){
+                reject(error);
+            }
+            else{
+                resolve();
+            }
+        })
+    })
+  }
+  const response = () => {
+    res.status(200).json({
+        data : null,
+        message : "특강 신청에 성공하였습니다."
+    })
+  }
+
+  const err = (error) => {
+    console.log(error);
+    res.status(400).json({
+        data : null,
+        message : "특강 신청에 실패하였습니다."
+    })
+  }
+  tokendecode()
+  .then(insertQuery)
+  .then(response)
+  .catch(err)
 });
 
 router.delete('/proposal/cancel' , (req, res, next) => {
 
 })
+
+router.get('/')
 
 
 
